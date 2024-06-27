94dd91be2cb74b0623f54dfdc78b6fcbfdbabdfc
MadOtacu <pipboy1917@gmail.com>
2024-06-27 17:18:45 +0400

    General improvements

--- Task1.go
+++ Task1.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -12,24 +12,46 @@ import (
 	"time"
 )
 
-var src = flag.String("src", "", "Ссылка на файл с сылками")
+var src = flag.String("src", "", "Ссылка на файл с сcылками")
 
-var dst = flag.String("dst", "", "Путь для записи с файлов")
+var dst = flag.String("dst", "", "Путь для записи файлов")
+
+func connect(element string) (*bytes.Buffer, error) {
+	if !strings.HasPrefix(element, "https://") {
+		return nil, errors.New("Invalid link")
+	}
+
+	data := bytes.Buffer{}
+
+	resp, errServ := http.Get(element)
+	if errServ != nil {
+		fmt.Println("Хост " + element + " не отвечает")
+		return nil, errors.New("Host is not responding")
+	}
+	defer resp.Body.Close()
+
+	copyErr := resp.Write(&data)
+	if copyErr != nil {
+		panic(copyErr)
+	}
+	return &data, nil
+}
 
 func main() {
 	start := time.Now()
-	flag.PrintDefaults()
 	flag.Parse()
 
 	var byteText, err = os.ReadFile(*src)
 	if err != nil {
-		panic(err)
+		flag.PrintDefaults()
+		os.Exit(2)
 	}
 
 	if _, errCheck := os.Stat(*dst); os.IsNotExist(errCheck) {
 		errCreation := os.Mkdir(*dst, 0777)
 		if errCreation != nil {
-			panic(errCreation)
+			flag.PrintDefaults()
+			os.Exit(2)
 		}
 	}
 
@@ -37,31 +59,19 @@ func main() {
 	textArr := strings.Split(text, "\n")
 
 	for i, element := range textArr {
-		if !strings.HasPrefix(element, "https://") {
+		temp, connectErr := connect(element)
+		if connectErr != nil {
 			continue
-		}
-		var temp bytes.Buffer
-
-		resp, errServ := http.Get(element)
-		if errServ != nil {
-			fmt.Println("Хост " + element + " не отвечает")
-			continue
-		}
-
-		copyErr := resp.Write(&temp)
-		if copyErr != nil {
-			panic(copyErr)
 		}
 
 		num := strconv.Itoa(i)
 		fmt.Println("Запись данных из " + element + " в файл " + num + ".html")
-		writeErr := ioutil.WriteFile(*dst+num+".html", temp.Bytes(), 0777)
+		writeErr := os.WriteFile(*dst+num+".html", temp.Bytes(), 0777)
 		if writeErr != nil {
 			panic(writeErr)
 		}
 
 		fmt.Println("В файл " + num + ".html сохранен шаблон сайта " + element)
-		resp.Body.Close()
 	}
 	elapsed := time.Since(start)
 	fmt.Println("Время выполнения программы:", elapsed)
