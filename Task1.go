6737c063f43be340b798b91a1c205b705d1a223f
MadOtacu <pipboy1917@gmail.com>
2024-06-27 15:57:39 +0400

    Updates & bug fixes

--- Task1.go
+++ Task1.go
@@ -9,41 +9,60 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
-var src = flag.String("src", "", "Input file path")
-var dst = flag.String("dst", "", "Output file path")
+var src = flag.String("src", "", "Ссылка на файл с сылками")
 
-func errHandler(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+var dst = flag.String("dst", "", "Путь для записи с файлов")
 
 func main() {
+	start := time.Now()
+	flag.PrintDefaults()
 	flag.Parse()
+
 	var byteText, err = os.ReadFile(*src)
-	errHandler(err)
-	text := string(byteText[:])
-	fmt.Println(text)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, errCheck := os.Stat(*dst); os.IsNotExist(errCheck) {
+		errCreation := os.Mkdir(*dst, 0777)
+		if errCreation != nil {
+			panic(errCreation)
+		}
+	}
+
+	text := string(byteText)
 	textArr := strings.Split(text, "\n")
 
 	for i, element := range textArr {
-		if strings.HasPrefix(element, "https://") {
-			var temp bytes.Buffer
-
-			resp, errServ := http.Get(element)
-			errHandler(errServ)
-
-			copyErr := resp.Write(&temp)
-			errHandler(copyErr)
-
-			num := strconv.Itoa(i)
-			fmt.Println(num)
-			writeErr := ioutil.WriteFile(*dst+num+".html", temp.Bytes(), 0666)
-			errHandler(writeErr)
-
-			fmt.Println(element)
+		if !strings.HasPrefix(element, "https://") {
+			continue
 		}
+		var temp bytes.Buffer
+
+		resp, errServ := http.Get(element)
+		if errServ != nil {
+			fmt.Println("Хост " + element + " не отвечает")
+			continue
+		}
+
+		copyErr := resp.Write(&temp)
+		if copyErr != nil {
+			panic(copyErr)
+		}
+
+		num := strconv.Itoa(i)
+		fmt.Println("Запись данных из " + element + " в файл " + num + ".html")
+		writeErr := ioutil.WriteFile(*dst+num+".html", temp.Bytes(), 0777)
+		if writeErr != nil {
+			panic(writeErr)
+		}
+
+		fmt.Println("В файл " + num + ".html сохранен шаблон сайта " + element)
+		resp.Body.Close()
 	}
+	elapsed := time.Since(start)
+	fmt.Println("Время выполнения программы:", elapsed)
 }
